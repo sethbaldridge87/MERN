e816f36afc11bb92cfabc59f846485f68355b33b
Seth Baldridge <sethbaldridge87@gmail.com>
2019-04-04 15:23:06 -0600

    Search results rendered

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Nav from "./components/Nav";
 import Parallax from "./components/Parallax";
 import Form from "./components/Form";
+import {BookList, BookListItem} from "./components/BookList";
 import API from "./utils/API";
 import "./App.css";
 
@@ -14,7 +15,6 @@ class App extends Component {
 
   handleFormSubmit = (event) => {
     event.preventDefault();
-    // console.log(this.state.bookSearch);
     API.getBooks(this.state.bookSearch)
       .then(res => {
         console.log(res);
@@ -41,7 +41,24 @@ class App extends Component {
         <Form
           onClick={this.handleFormSubmit}
           onChange={this.handleInputChange}
-        />
+        /><br />
+        {!this.state.books.length ? (
+          <h1 id="noBook">No Books to Display</h1>
+        ) : (
+          <BookList>
+            {this.state.books.map(book => {
+              return (
+                <BookListItem
+                  title={book.volumeInfo.title}
+                  authors={book.volumeInfo.authors}
+                  description={book.volumeInfo.description}
+                  image={book.volumeInfo.imageLinks.smallThumbnail}
+                  link={book.volumeInfo.infoLink}
+                />
+              )
+            })}
+          </ BookList>
+        )}
       </div>
     )
 
