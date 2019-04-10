b2f0b093b8cf0d810f607c1c2fd9b9350449ed61
sethbaldridge87 <sethbaldridge87@gmail.com>
2019-04-09 20:35:46 -0600

    Console log connection

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("*", (req, res) => {
 });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/MERN_books");
-
+console.log(process.env.MONGODB_URI);
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
