269d1ee92b541c1c86169aacf75471d3532ab6a1
sethbaldridge87 <sethbaldridge87@gmail.com>
2019-04-09 21:21:02 -0600

    NPM Build

---
+++ client/build/precache-manifest.f92525c6386ef19c17b3007773eb1eef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9634331a3a70cc410aba",
+    "url": "/static/js/main.df9b97b2.chunk.js"
+  },
+  {
+    "revision": "d7890ba8286a24933f32",
+    "url": "/static/js/2.1624730c.chunk.js"
+  },
+  {
+    "revision": "9634331a3a70cc410aba",
+    "url": "/static/css/main.809f220a.chunk.css"
+  },
+  {
+    "revision": "73b5e50213487fb7d86fcb83a5af51e1",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 // Define API routes here
@@ -18,11 +18,11 @@ app.use("/api", apiRoutes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/MERN_books", (err)=> console.log(err));
-console.log(process.env);
+// console.log(process.env);
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
